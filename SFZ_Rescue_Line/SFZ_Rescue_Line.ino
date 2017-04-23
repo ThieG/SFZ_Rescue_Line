9c51fb21072d5be2b6f1d237d731013e5ae78c2b
ThieG <martina_matthias@gmx.de>
2017-04-23 15:30:02 +0200

    Initialer Arduino Skech

---
+++ SFZ_Rescue_Line/SFZ_Rescue_Line.ino
@@ -0,0 +1,85 @@
+/*!
+****************************************************************************************************
+* \file      SFZ_Rescue_Line.ino
+*
+* \ingroup   RESCUE_LINE
+*
+*            Arduino Software als Beispiel, wie so ein Programm funktionieren und aussehen kann.
+*
+****************************************************************************************************
+*/
+/*!
+****************************************************************************************************
+* \defgroup RESCUE_LINE     Robocup Rescue Line
+****************************************************************************************************
+*/
+#include  "MeAuriga.h"
+
+
+#define SERVO_LENKUNG_MITTE   90u  /**< Legt die Mittelstellung der Lenkung fest, also Geradeausfahren! */
+#define SERVO_RECHTS_MAX       0u  /**< Legt den maximalen Ausschlag nach rechts fest! */
+#define SERVO_LINKS_MAX      170u  /**< Legt den maximalen Ausschlag nach links fest! */
+
+/*------------------------------------------------------------------------------------------------*/
+/*!
+* \brief    Aktoren 
+*/
+/*------------------------------------------------------------------------------------------------*/
+MeDCMotor MotorAntrieb(M1);
+
+MePort ServoPort(PORT_6);
+int16_t ServoLenkungPin = ServoPort.pin1();
+Servo ServoLenkung;
+
+MeRGBLed RGBLedOnBoard(0, 12);
+
+/*------------------------------------------------------------------------------------------------*/
+/*!
+* \brief    Sensoren
+*/
+/*------------------------------------------------------------------------------------------------*/
+MeGyro GyroOnBoard(0, 0x69);
+
+
+/*------------------------------------------------------------------------------------------------*/
+/*!
+* \brief    Globale Variablen und Funktionsprototypen die man hier halt so braucht!
+*/
+/*------------------------------------------------------------------------------------------------*/
+
+/*------------------------------------------------------------------------------------------------*/
+/*!
+* \brief     Arduino Setup
+*
+*            Wird beim Start des Programms (entweder nach dem uebertragen auf das Board oder 
+*            nach Druecken des Reset-Tasters) einmalig aufgerufen, 
+*            um z. B. Pins als Eingang oder Ausgang zu definieren.
+*/
+/*------------------------------------------------------------------------------------------------*/
+void setup() {
+
+  ServoLenkung.attach(ServoLenkungPin);
+  ServoLenkung.write(SERVO_LENKUNG_MITTE);
+
+  MotorAntrieb.stop();
+
+  RGBLedOnBoard.setColor(0, 150, 150, 150); /* 0 -> Alle RGBs anschalten, ansonsten 1 bis 12.... */
+  RGBLedOnBoard.show();
+
+  GyroOnBoard.begin();
+
+}
+
+/*------------------------------------------------------------------------------------------------*/
+/*!
+* \brief     Arduino Main Loop
+*
+*            Wird durchgehend immer wieder durchlaufen, solange das Arduino-Board eingeschaltet ist.
+*/
+/*------------------------------------------------------------------------------------------------*/
+void loop() {
+
+  GyroOnBoard.update();
+}
+
+
